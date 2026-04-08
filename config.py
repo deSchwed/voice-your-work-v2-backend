103cd66fe7d5d1eaf5126a41fc3bb0c1fe415620
Lucas Mikael Roni Suomela <lucasmikaelroni.suomela@uzh.ch>
2026-04-08 16:24:48 +0200

    Rework project structure with uv

---
+++ config.py
@@ -0,0 +1,21 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    database_url: str
+
+    secret_key: SecretStr
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    reset_token_expire_minutes: int = 30
+
+    # TODO: Mail server config
+
+
+settings = Settings()  # type: ignore[call-arg]  # Loaded from .env file
